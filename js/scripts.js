99586d14afef9235f88603dd56f1ea93135ce526
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-03 10:18:12 -0700

    edited business logic function and called it within the UI logic function to correctly out put the right results

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,9 @@
 function python() {
   const qOne = $("input:radio[name=question1]:checked").val();
   const qTwo = $("input:radio[name=question2]:checked").val();
-  const pythonResult = 'Python';
-
+ 
   if (qOne === 'yes' && qTwo === 'desert' || qOne === 'yes' && qTwo === 'tropical') {
-    return pythonResult;
+    return true;
   };
 };
 
@@ -14,38 +13,35 @@ function swift() {
   const qOne = $("input:radio[name=question1]:checked").val();
   const qThree = $("input:radio[name=question3]:checked").val();
   const qFour = $("input:radio[name=question4]:checked").val();
-  const swiftResult = 'Swift';
-
+  
   if (qOne === 'no' && qFour === 'mac'|| qOne === 'no' && qThree === 'boba') {
-    return swiftResult;
+    return true;
   };
 };
 
 function cSharp() {
   const qOne = $("input:radio[name=question1]:checked").val();
   const qFive = $("input:radio[name=question5]:checked").val();
-  const cSharpResult = 'C#';
 
   if (qOne === 'no' && qFive === 'tofu' || qOne === 'no' && qFive === 'sunny') {
-    return cSharpResult;
+    return true;
   };
 };
 
 function finalSelector() {
-  if (python === true) {
-    return python();
-  } else if (swift === true) {
-    return swift();
-  } else if (cSharp === true) {
-    return cSharp();
+  const snake = python();
+  const iphone = swift();
+  const sharp = cSharp();
+  if (snake === true) {
+    return 'Python';
+  } else if (iphone === true) {
+    return 'Swift';
+  } else if (sharp === true) {
+    return 'C#';
   } else {
     return 'JavaScript';
   }
 };
-
-
-
-
 
 
 //UI Logic
@@ -54,9 +50,10 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     const nameInput = $('input#person1').val()
+   
     
     $('.nameOutput').text(nameInput);
-    $('.langOutput').text(Python());
+    $('.langOutput').text(finalSelector());
     $('.outputContainer').show();
   })
 })
