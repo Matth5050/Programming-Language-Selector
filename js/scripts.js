a22c0f908ccd02bac585739b8ec0d0550e3d4f11
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-03 09:53:23 -0700

    added second function to select for C#

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
 //Business Logic
 
-function Python() {
+function python() {
   const qOne = $("input:radio[name=question1]:checked").val();
   const qTwo = $("input:radio[name=question2]:checked").val();
-  const qThree = $("input:radio[name=question3]:checked").val();
-  const qFour = $("input:radio[name=question4]:checked").val();
-  const qFive = $("input:radio[name=question5]:checked").val();
-  const pythonResult = 'Python'
+  const pythonResult = 'Python';
 
   if (qOne === 'yes' && qTwo === 'desert' || qOne === 'yes' && qTwo === 'tropical') {
     return pythonResult;
   };
 };
 
-function Swift() {
+function swift() {
   const qOne = $("input:radio[name=question1]:checked").val();
   const qThree = $("input:radio[name=question3]:checked").val();
   const qFour = $("input:radio[name=question4]:checked").val();
-  const swiftResult = 'Swift'
+  const swiftResult = 'Swift';
 
   if (qOne === 'no' && qFour === 'mac'|| qOne === 'no' && qThree === 'boba') {
     return swiftResult;
+  };
+};
+
+function cSharp() {
+  const qOne = $("input:radio[name=question1]:checked").val();
+  const qFive = $("input:radio[name=question5]:checked").val();
+  const cSharpResult = 'C#';
+
+  if (qOne === 'no' && qFive === 'tofu' || qOne === 'no' && qFive === 'sunny') {
+    return cSharpResult;
   };
 };
 
